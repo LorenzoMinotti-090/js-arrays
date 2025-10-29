6f2d976422f7cf454257310bd87e231911c67011
LorenzoMinotti-090 <minottilorenzo@gmail.com>
2025-10-29 15:12:22 +0100

    feat: punto 1

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,7 +10,7 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = null;
+const fourthTeacher = teachers[3];
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
@@ -31,3 +31,6 @@ const lewisIndex = null;
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
 const isTeachersEmpty = null;
+
+
+console.log(fourthTeacher);
